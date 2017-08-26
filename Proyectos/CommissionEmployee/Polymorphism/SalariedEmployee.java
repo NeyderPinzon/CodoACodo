a45ece0664e3de042102205929247577d27318b0
Esteban Borai <leonardoestebanbba@gmail.com>
2017-08-26 12:33:15 -0300

    Create SalariedEmployee.java

---
+++ Proyectos/CommissionEmployee/Polymorphism/SalariedEmployee.java
@@ -0,0 +1,35 @@
+package com.codoacodo;
+
+public class SalariedEmployee extends Employee {
+    
+    private double weeklySalary;
+    
+    public SalariedEmployee (String firstName, String lastName, String socialSecurityNumber,
+            double weeklySalary){
+        super(firstName, lastName, socialSecurityNumber);
+        setWeeklySalary(weeklySalary);
+    }
+    
+    public String toString() {
+        return String.format("Salaried Employee: %s \n%s: $%,.2f",
+                super.toString(), "weekly salary", getWeeklySalary());
+    }
+    
+    public void setWeeklySalary(double weeklySalary){
+        if (weeklySalary > 0.0){
+            this.weeklySalary = weeklySalary;
+        } else {
+            throw new IllegalArgumentException(
+                "Weekly Salary must be >= 0.0");
+        }
+    }
+    
+    public double getWeeklySalary (){
+        return this.weeklySalary;
+    }
+
+    @Override
+    public double earnings() {
+        return getWeeklySalary();
+    }   
+}
