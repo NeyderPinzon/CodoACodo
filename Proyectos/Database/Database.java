7d7986d1cdd54014c9d226d47817f843c2040e67
Esteban Borai <leonardoestebanbba@gmail.com>
2017-08-29 21:06:25 -0300

    Update Database.java

--- Proyectos/Database/Database.java
+++ Proyectos/Database/Database.java
@@ -1,28 +1,55 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package com.codoacodo;
+package com.codoacodo.gjallarhorn;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import javax.naming.OperationNotSupportedException;
 
 /**
- *
- * @author alumno
+ * Acceso a la Creacion de la Base de Datos
+ * @author Yo
  */
+
 public class Database {
 
     private Connection conexion;
             
     public Database(String url, String pass, String base, String user)throws OperationNotSupportedException {
-         conexion = createConnection(url, base, user, pass);
+        conexion = createConnection(url, base, user, pass);
     }
 
+    public void setup() {
+        cargarPersona();
+        cargarLegajo();
+    }
+    /**
+     * Método encargado de crear la tabla "Legajo"
+     */
+    private void cargarLegajo() {
+        StringBuffer buffer = new StringBuffer("id INT NOT NULL AUTO_INCREMENT,");
+        buffer.append("cuit VARCHAR(20),").append("pesona_id INT,")
+                .append("PRIMARY KEY (id),").append("FOREING KEY (persona_id),")
+                .append("REFERENCES persona(id)");
+    }
     
+    /**
+     * Metodo encargado de cear la tabla "Persona"
+     */
+    private void cargarPersona() {
+        StringBuffer buffer = new StringBuffer("id INT NOT NULL AUTO_INCREMENT,");
+                buffer.append("edad INT,").append("apellido VARCHAR(20),")
+                .append("nombre VARCHAR(20),").append("PRIMARY KEY (id)");
+        
+        createTable("persona", buffer.toString());
+        
+        String nombreTabla = "persona";
+        String atributos = "apellido, nombre, edad";
+
+        insertData(nombreTabla, atributos, "\"Aladro\", \"Juan\", 65");
+        insertData(nombreTabla, atributos, "\"Aladro\", \"Alfredo\", 35");
+        
+    }
     
+        
     private boolean createTable(String nombre, String atributos) {
         try {
             
