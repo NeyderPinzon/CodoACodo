413362a6ec4d21714a16df59690badc8ea71cbec
Esteban Borai <leonardoestebanbba@gmail.com>
2017-08-30 19:37:35 -0300

    Create Main.java

---
+++ Ejercicios/Respuestas/Guia de Ejercicios 5/5.3/Main.java
@@ -0,0 +1,10 @@
+package com.codoacodo;
+
+public class Main {
+
+    public static void main(String[] args) {
+        TablaDeMultiplicar tabla = new TablaDeMultiplicar(0);
+        tabla.init();
+    }
+    
+}
