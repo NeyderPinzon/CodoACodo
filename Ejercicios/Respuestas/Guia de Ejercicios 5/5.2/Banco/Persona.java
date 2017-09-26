351d64d2cfada0c4605246c9ac3af72768d16b04
Esteban Borai <leonardoestebanbba@gmail.com>
2017-09-26 16:29:03 -0300

    Create Persona.java

---
+++ Ejercicios/Respuestas/Guia de Ejercicios 5/5.2/Banco/Persona.java
@@ -0,0 +1,56 @@
+package com.codoacodo;
+
+class Persona {
+    
+    private String firstName;
+    private double [] account = new double [2];
+    private String [] passwords = new String [5];
+    
+    public Persona (String firstName, double pesos, double dolar, 
+            String password) {
+        this.firstName = firstName;
+        this.account[0] = pesos;
+        this.account[1] = dolar;
+        this.passwords[0] = password;
+    }
+    
+    public String getFirstName(){
+        return this.firstName;
+    }
+    
+    public void setFirstName(String firstName){
+        this.firstName = firstName;
+    }
+    
+    public double getBalancePesos() {
+        return this.account[0];
+    }
+    
+    public double getBalanceDolar() {
+        return this.account[1];
+    }
+    
+    public void setBalance(double balance, int account) {
+        switch (account) {
+            case 0:
+                this.account[0] = balance;
+                break;
+            case 1:
+                this.account[1] = balance;
+                break;
+            default:
+                this.account[0] = balance;
+                break;
+        }
+    }
+    
+    public String toString() {
+        return String.format("Account: %s\nBalance:\nAR$: %.2f\nUS$: %.2f\n",
+                getFirstName(), getBalancePesos(), getBalanceDolar());
+    }
+
+    public String [] getPassword() {
+        return this.passwords;
+    }
+    
+}
